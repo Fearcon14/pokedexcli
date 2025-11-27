4b2509d05542b168b33a713fe42d2d2edb4b7a46
Kevin Sinn <kevinsinn01@gmail.com>
2025-11-27 14:32:38 +0100

    Input loop added

--- main.go
+++ main.go
@@ -2,8 +2,21 @@ package main
 
 import (
 	"fmt"
+	"bufio"
+	"os"
 )
 
 func main() {
-	fmt.Println("Hello, World!")
+	for {
+		fmt.Print("Pokedex > ")
+		scanner := bufio.NewScanner(os.Stdin)
+		scanner.Scan()
+		text := scanner.Text()
+		cleaned := cleanInput(text)
+		if len(cleaned) == 0 {
+			continue
+		}
+		command := cleaned[0]
+		fmt.Printf("Your command was: %s\n", command)
+	}
 }
